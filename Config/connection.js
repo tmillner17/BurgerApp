b71a2aa84e1cc45f54a64e21a419660d8890679b
tyler <tmillner17@gmail.com>
2019-08-29 18:31:53 -0600

    heroku commit

--- Config/connection.js
+++ Config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql")
 
 var connection = mysql.createConnection ({
-    host: "localhost",
+    host: "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: "gko34zzfwy52avyv",
+    password: "cr5yvjghytjkwgdu",
+    database: "`kk0s6y3x9gw4rwoa`"
 });
 
 if (process.env.JAWSDB_URL) {
